2b5a890aa7e1599d62abf8977a53d06aac6dbcfa
zhouxiao <919486216@qq.com>
2018-06-22 02:32:08 +0800

    2018.6.21

--- pages/chatroom_teacher/list/list.js
+++ pages/chatroom_teacher/list/list.js
@@ -1,5 +1,13 @@
 // pages/chatroom_teacher/list/list.js
 var app = getApp()
+/*const recorderManager = wx.getRecorderManager()
+const options = {
+  duration: 600000,
+  sampleRate: 44100,
+  numberOfChannels: 1,
+  encodeBitRate: 192000,
+  format: 'mp3'
+}*/
 Page({
 
   /**
