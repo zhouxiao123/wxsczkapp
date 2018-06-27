319886468c2d1b87dfe5333fc0adbec80f6c7949
huangyuepeng <565699084@qq.com>
2018-06-27 13:03:53 +0800

    6.27

--- pages/zhiyuan_shenhe/zhiyuan_shenhe_tishi/zhiyuan_shenhe_tishi.js
+++ pages/zhiyuan_shenhe/zhiyuan_shenhe_tishi/zhiyuan_shenhe_tishi.js
@@ -13,7 +13,93 @@ Page({
    */
   onLoad: function (options) {
     wx.setNavigationBarTitle({ title: "志愿填报方案评估功能使用提示" })
-
+    /**
+    * 加载的时候获取微信id
+    */
+    var that = this
+    var value = wx.getStorageSync('oid')
+    //console.log(value)
+    if (value) {
+      that.setData({ oid: value })
+    } else {
+      wx.login({
+        success: function (res) {
+          if (res.code) {
+            //console.log(res);
+            //发起网络请求
+            wx.request({
+              url: app.globalData.baseUrl + 'wx/login',
+              data: {
+                code: res.code
+              },
+              success: function (res) {
+                if (res.data == "") {
+                  wx.showModal({
+                    title: '提示',
+                    content: '获取用户登录信息失败',
+                    showCancel: false,
+                    success: function (res) {
+                      if (res.confirm) {
+                        console.log('用户点击确定')
+                      } else if (res.cancel) {
+                        console.log('用户点击取消')
+                      }
+                    }
+                  })
+                }
+                wx.setStorageSync('oid', res.data)
+                //继续处理上面的
+                that.setData({ oid: res.data })
+              }
+            })
+          } else {
+            console.log('获取用户登录态失败！' + res.errMsg)
+            wx.showModal({
+              title: '提示',
+              content: '获取用户登录状态失败',
+              showCancel: false,
+              success: function (res) {
+                if (res.confirm) {
+                  console.log('用户点击确定')
+                } else if (res.cancel) {
+                  console.log('用户点击取消')
+                }
+              }
+            })
+          }
+        }
+      })
+    }
+    /*
+        * 调用getUserDetail方法，通过oid查询对应的user集合
+        */
+    wx.request({
+      url: app.globalData.baseUrl + 'wx/getUserDetail',
+      data: {
+        oid: that.data.oid
+      },
+      success: function (res) {
+        console.log(res.data)
+        if (res.data.length == "0") {
+          wx.showModal({
+            title: '提示',
+            content: '请先填写资料',
+            showCancel: false,
+            success: function (res) {
+              wx.navigateTo({
+                url: '/pages/personal_info/personal_info'
+              })
+            }
+          })
+        } else {
+          that.setData({
+            user: res.data
+          })
+          that.data.userid = res.data.id
+          console.log(that.data.userid)
+        }
+      }
+    })
   },
    /**
    * 点击跳转提交方式选择
@@ -23,8 +109,28 @@ Page({
       mask: true,
       title: '加载中'
     })
-    wx.navigateTo({
-      url: '/pages/zhiyuan_shenhe/tijiao_fangshi/tijiao_fangshi_xuanze/tijiao_fangshi_xuanze'
+    var that = this
+    wx.request({
+      url: app.globalData.baseUrl + 'wx/shenhe_stop_or_start',
+      data: {
+        userid: that.data.userid
+      },
+      success: function (res) {
+        console.log(res.data)
+        if (res.data.result == "yizhifu") {
+          wx.navigateTo({
+            url: '/pages/zhiyuan_shenhe/tijiao_fangshi/tijiao_fangshi_xuanze/tijiao_fangshi_xuanze'
+          })
+        } else if (res.data.result == "ok"){
+          wx.navigateTo({
+            url: '/pages/zhiyuan_shenhe/tijiao_fangshi/tijiao_fangshi_xuanze/tijiao_fangshi_xuanze'
+          })
+        } else if (res.data.result == "stop") {
+          wx.navigateTo({
+            url: '/pages/zhiyuan_shenhe/stop_zhiyuan/stop_zhiyuan'
+          })
+        }
+      }
     })
     wx.hideLoading()
   },
